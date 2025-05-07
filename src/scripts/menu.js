75a3ae6ca2381e7a74853deed74535abdeb7256c
KonstantinosDiamantogiannis <tsingoku@yahoo.gr>
2025-05-07 17:26:53 +0300

    Final changes: Tags and Astro Islands

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,19 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
-  });
+
+// document.querySelector('.hamburger').addEventListener('click', () => {
+//    document.querySelector('.nav-links').classList.toggle('expanded'); }); 
+ 
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  if (hamburger && navLinks) {
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('expanded');
+    });
+  }
+});
+
+
+
+// console.log(document.querySelector('.hamburger'));
+// console.log(document.querySelector('.nav-links'));
